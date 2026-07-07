ddbc9545da6cbfbd6bfe356976379656099370a0
kaywambui-1 <kamauchristine85@gmail.com>
2026-07-07 19:10:03 +0300

    update  main.js and numberOperations.js

---
+++ numberOperations.js
@@ -0,0 +1,15 @@
+// Basic math
+let a = 10;
+let b = 3;
+
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // Division
+console.log(a % b); // Modulus (remainder)
+console.log(a ** b); // Exponentiation 
+
+//increment and decrement
+let count = 0;
+count++; // count is now 1
+count--; // count is now 0

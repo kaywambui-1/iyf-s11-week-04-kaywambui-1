b749d499b3fbc6398e54652dcd15c09d19585624
kaywambui-1 <kamauchristine85@gmail.com>
2026-07-08 16:27:03 +0300

    update functions.js

--- functions.js
+++ functions.js
@@ -18,3 +18,9 @@ const divide = (a, b) => {
     }
     return a / b;
 };
+
+console.log(greet("Christine")); // Output: Hello, Christine!
+console.log(add(5, 3)); // Output: 8
+console.log(multiply(4, 6)); // Output: 24
+console.log(divide(10, 2)); // Output: 5
+console.log(divide(10, 0)); // Output: Cannot divide by zero
